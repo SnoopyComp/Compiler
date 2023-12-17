034e03380c7515aba53f20d7fb529961eefd0379
w3schools-test <test@w3schools.com>
2023-12-18 00:22:45 +0900

    CallExpr update

--- Semantic/analyze.c
+++ Semantic/analyze.c
@@ -396,6 +396,16 @@ static void checkNode(TreeNode *t)
 			// Semantic Error: Invalid Arguments
 			TreeNode *paramNode = calleeSymbol->node->child[0];
 			TreeNode *argNode = t->child[0];
+			//void argment with non-null parameter
+			if(argNode->flag==TRUE && paramNode!=NULL)InvalidFunctionCallError(t->name,t->lineno);
+			//non-void argment with null parameter
+			else if( argNode->flag==FALSE && paramNode==NULL) InvalidFunctionCallError(t->name,t->lineno);
+			do{
+				if(paramNode->type != argNode->type) InvalidFunctionCallError(t->name,t->lineno);
+				paramNode=paramNode->sibling;
+				argNode=argNode->sibling;
+			} while(paramNode->sibling || argNode->sibling)
+					
 			/*********************Fill the Code*************************
 			 *                                                         *
 			 *                                                         *
